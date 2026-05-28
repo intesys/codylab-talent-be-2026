748e10713057542b58a653bed128056b216db970
nicolaltafini <nicola.altafini06@gmail.com>
2026-05-28 14:58:07 +0200

    es 3.3

--- it/intesys/codylab/MyFirstCodyLabApplication.java
+++ it/intesys/codylab/MyFirstCodyLabApplication.java
@@ -1,6 +1,6 @@
 package it.intesys.codylab;
 
-import it.intesys.codylab.formageometrica.Cerchio;
+import it.intesys.codylab.formageometrica.Quadrato;
 import it.intesys.codylab.messaggio.Messaggio;
 import it.intesys.codylab.messaggio.MessaggioFormaGeometrica;
 import it.intesys.codylab.messaggio.MessaggioStatico;
@@ -14,8 +14,7 @@ public class MyFirstCodyLabApplication {
 
     private static void stampa() {
         // Messaggio mess = new MessaggioStatico("Betty");
-        // Messaggio messaggio = new MessaggioFormaGeometrica(new Cerchio(5.0f));
-        Messaggio messaggio = new MessaggioFormaGeometrica(new Cerchio(6.0f));
+        Messaggio messaggio = new MessaggioFormaGeometrica(new Quadrato(5.0f));
         System.out.println(messaggio.messaggio());
     }
 }
