c41149effd4e7d7cf2bde58ea136951e8831903b
Alessandro Pozzato <153738740+alencrystal@users.noreply.github.com>
2026-05-28 15:01:19 +0200

    Aggiunta classe Rettangolo e relativa call nel metodo stampa di MyFirstCodyLabApplication

--- it/intesys/codylab/MyFirstCodyLabApplication.java
+++ it/intesys/codylab/MyFirstCodyLabApplication.java
@@ -14,9 +14,10 @@ public class MyFirstCodyLabApplication {
 // aggiuto static al metodo stampa per poterlo chiamare dal main
     private static void stampa() {
         //Messaggio mess = new MessaggioStatico("Betty");
-         //Messaggio mess = new MessaggioFormaGeometrica(new Cerchio(5.0f));
+        //Messaggio mess = new MessaggioFormaGeometrica(new Cerchio(5.0f));
         //Messaggio mess = new MessaggioFormaGeometrica(new Cerchio(6.0f));
-        Messaggio mess = new MessaggioFormaGeometrica(new Quadrato(5.0f));
+        //Messaggio mess = new MessaggioFormaGeometrica(new Quadrato(5.0f));
+        Messaggio mess = new MessaggioFormaGeometrica(new Rettangolo(5.0f, 3.0f));
         System.out.println(mess.messaggio());
     }
 }
