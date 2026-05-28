406e4f1ac618b6d20ae85b8be8fb93649c5a71cf
Nicolas Taddei <il.taddee@gmail.com>
2026-05-28 15:16:36 +0200

    es. 3.3

--- it/intesys/codylab/MyFirstCodyLabApplication.java
+++ it/intesys/codylab/MyFirstCodyLabApplication.java
@@ -2,6 +2,8 @@ package it.intesys.codylab;
 
 import it.intesys.codylab.messaggio.Messaggio;
 import it.intesys.codylab.messaggio.MessaggioStatico;
+import it.intesys.codylab.messaggio.MessaggioFormaGeometrica;
+import it.intesys.codylab.formageometrica.Quadrato;
 
 public class MyFirstCodyLabApplication {
 
@@ -11,9 +13,10 @@ public class MyFirstCodyLabApplication {
     }
 
     private static void stampa() {
-        Messaggio mess = new MessaggioStatico("Betty");
-        // Messaggio messaggio = new MessaggioFormaGeometrica(new Cerchio(5.0f));
-        // Messaggio messaggio = new MessaggioFormaGeometrica(new Cerchio(6.0f));
+        // Messaggio mess = new MessaggioStatico("Betty");
+        // Messaggio mess = new MessaggioFormaGeometrica(new Cerchio(5.0f));
+        // Messaggio mess = new MessaggioFormaGeometrica(new Cerchio(6.0f));
+        Messaggio mess = new MessaggioFormaGeometrica(new Quadrato(5.0f));
         System.out.println(mess.messaggio());
     }
 }
