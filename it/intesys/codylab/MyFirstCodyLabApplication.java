5704417e39ed0f9f0572cc301b2bde672c9940dd
leonardosanketh2004-blip <leonardosanketh2004@gmail.com>
2026-05-28 14:35:35 +0200

    Primo esercizio java

--- it/intesys/codylab/MyFirstCodyLabApplication.java
+++ it/intesys/codylab/MyFirstCodyLabApplication.java
@@ -1,6 +1,8 @@
 package it.intesys.codylab;
 
+import it.intesys.codylab.formageometrica.Cerchio;
 import it.intesys.codylab.messaggio.Messaggio;
+import it.intesys.codylab.messaggio.MessaggioFormaGeometrica;
 import it.intesys.codylab.messaggio.MessaggioStatico;
 
 public class MyFirstCodyLabApplication {
@@ -10,9 +12,15 @@ public class MyFirstCodyLabApplication {
         System.out.println("Ciao!");
     }
 
+<<<<<<< Updated upstream
     private void stampa() {
         Messaggio mess = new MessaggioStatico("Betty");
         // Messaggio messaggio = new MessaggioFormaGeometrica(new Cerchio(5.0f));
+=======
+    private static void stampa() {
+        //Messaggio messaggio = new MessaggioStatico();
+        Messaggio messaggio = new MessaggioFormaGeometrica(new Cerchio(5.0f));
+>>>>>>> Stashed changes
         // Messaggio messaggio = new MessaggioFormaGeometrica(new Cerchio(6.0f));
         System.out.println(mess.messaggio());
     }
