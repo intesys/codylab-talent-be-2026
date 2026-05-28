467742f74ad8af10d963025ea28f28520d3332ca
leonardosanketh2004-blip <leonardosanketh2004@gmail.com>
2026-05-28 15:04:07 +0200

    primo esercizio java

---
+++ it/intesys/codylab/formageometrica/Rettangolo.java
@@ -0,0 +1,23 @@
+package it.intesys.codylab.formageometrica;
+
+public class Rettangolo extends Quadrilatero {
+
+    private final float base;
+    private final float altezza;
+
+    public Rettangolo(float base, float altezza) {
+        super(base, altezza, base, altezza);
+        this.base = base;
+        this.altezza = altezza;
+    }
+
+    @Override
+    public float area() {
+        return base * altezza;
+    }
+
+    @Override
+    public String toString() {
+        return "Rettangolo con base " + base + ", altezza " + altezza;
+    }
+}
