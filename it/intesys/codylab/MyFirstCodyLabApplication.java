a418de098609702b4b82c8a3a0b96e9180e62d1b
Alessandro Pozzato <153738740+alencrystal@users.noreply.github.com>
2026-05-28 14:23:51 +0200

    sistemato gli import, aggiunto il messaggio per trovare l'area del quadrato

--- it/intesys/codylab/MyFirstCodyLabApplication.java
+++ it/intesys/codylab/MyFirstCodyLabApplication.java
@@ -1,7 +1,7 @@
 package it.intesys.codylab;
 
-import it.intesys.codylab.messaggio.Messaggio;
-import it.intesys.codylab.messaggio.MessaggioStatico;
+import it.intesys.codylab.messaggio.*;
+import it.intesys.codylab.formageometrica.*;
 
 public class MyFirstCodyLabApplication {
 
@@ -13,9 +13,10 @@ public class MyFirstCodyLabApplication {
 
 // aggiuto static al metodo stampa per poterlo chiamare dal main
     private static void stampa() {
-        Messaggio mess = new MessaggioStatico("Betty");
-        // Messaggio messaggio = new MessaggioFormaGeometrica(new Cerchio(5.0f));
-        // Messaggio messaggio = new MessaggioFormaGeometrica(new Cerchio(6.0f));
+        //Messaggio mess = new MessaggioStatico("Betty");
+         //Messaggio mess = new MessaggioFormaGeometrica(new Cerchio(5.0f));
+        //Messaggio mess = new MessaggioFormaGeometrica(new Cerchio(6.0f));
+        Messaggio mess = new MessaggioFormaGeometrica(new Quadrato(5.0f));
         System.out.println(mess.messaggio());
     }
 }
