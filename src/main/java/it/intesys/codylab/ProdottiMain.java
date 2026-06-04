43450b1a557b37b19229f7c613218b97785f75d1
Elisabetta Freschi <elisabetta.freschi@intesys.it>
2026-06-04 14:03:28 +0200

    Introdotto log

--- src/main/java/it/intesys/codylab/ProdottiMain.java
+++ src/main/java/it/intesys/codylab/ProdottiMain.java
@@ -2,27 +2,31 @@ package it.intesys.codylab;
 
 import it.intesys.codylab.prodotto.Prodotto;
 import it.intesys.codylab.prodotto.ProdottoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Optional;
 
 public class ProdottiMain {
 
+    private static final Logger logger = LoggerFactory.getLogger(ProdottiMain.class);
+
     public static void main(String[] args) {
         catalogoProdotti();
     }
 
     public static void catalogoProdotti() {
-        System.out.println("Inizio");
+        logger.debug("Inizio");
         ProdottoService prodottoService = new ProdottoService();
         prodottoService.addProdotto("Mouse", 30.0f);
         prodottoService.addProdotto("Tastiera", 15.0f);
         prodottoService.addProdotto("Cuffie", 25.0f);
 
         List<Prodotto> prodotti = prodottoService.getProdotti();
-        System.out.println("Trovati i prodotti:");
+        logger.info("*** Trovati prodotti: ");
         for (Prodotto prodotto : prodotti) {
-            System.out.println(prodotto);
+            logger.info("**" + prodotto);
         }
 
         prodottoService.updateProdotto(1, "Mouse wireless", 45.0f);
@@ -40,7 +44,7 @@ public class ProdottiMain {
         }
         Optional<Prodotto> prodottoCancellato = prodottoService.getProdottoById(2);
         if (prodottoCancellato.isPresent()) {
-            System.err.println("La cancellazione non è andata a buon fine");
+            logger.error("La cancellazione non è andata a buon fine");
         } else {
             System.out.println("Prodotto 2 cancellato correttamente");
         }
