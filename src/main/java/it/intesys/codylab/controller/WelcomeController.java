a77cf4980dcfcca9c160ceaba3393c726667dd8e
DanniaLima <dannia.lima@gmail.com>
2026-06-25 12:45:52 +0200

    test: @RestController and @GetMapping

---
+++ src/main/java/it/intesys/codylab/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package it.intesys.codylab.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping
+    public String welcomeMessage() {
+        return "Welcome";
+    }
+}
