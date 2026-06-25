3078cb4c3b225c13f44dde377778eccb53ad283b
leonardosanketh2004-blip <leonardosanketh2004@gmail.com>
2026-06-25 11:53:14 +0200

    Test ActivityRepositoryIntTest

---
+++ src/test/java/it/intesys/codylab/db/repository/ActivityRepositoryIntTest.java
@@ -0,0 +1,23 @@
+package it.intesys.codylab.db.repository;
+
+import it.intesys.codylab.db.config.HikariDataSourceProvider;
+import it.intesys.codylab.db.model.Activity;
+import org.junit.jupiter.api.Test;
+
+
+
+import javax.sql.DataSource;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ActivityRepositoryIntTest {
+
+    @Test
+    void findById() {
+        DataSource dataSource = HikariDataSourceProvider.getDataSource();
+        ActivityRepository activityRepository = new ActivityRepository(dataSource);
+        Optional<Activity> activity = activityRepository.findById(100L);
+        assert(activity.isEmpty());
+    }
+}
