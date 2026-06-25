d61f978f34cab0b7f5aa96c847cb95418a2c4723
nicolaltafini <nicola.altafini06@gmail.com>
2026-06-25 12:52:21 +0200

    pagina di benvenuto

---
+++ src/main/java/it/intesys/codylab/controller/WelcomeController.java
@@ -0,0 +1,21 @@
+package it.intesys.codylab.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping
+    public String welcomeMessage() {
+        return """
+<html>
+    <body>
+        <h1>CodyLab 404 Error Page</h1>
+        <p>ciao</p>
+        <div>There was an unexpected error (type=Not Found, status=404).</div>
+    </body>
+</html>
+                
+                """;
+    }
+}
