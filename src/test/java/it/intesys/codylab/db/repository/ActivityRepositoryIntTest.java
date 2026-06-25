0374a90b0c4509f983adb353dd11d3ec0e488463
nicolaltafini <nicola.altafini06@gmail.com>
2026-06-25 12:19:11 +0200

    server port

--- src/test/java/it/intesys/codylab/db/repository/ActivityRepositoryIntTest.java
+++ src/test/java/it/intesys/codylab/db/repository/ActivityRepositoryIntTest.java
@@ -11,19 +11,16 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ActivityRepositoryIntTest {
+    private ActivityRepository activityRepository = new ActivityRepository(HikariDataSourceProvider.getDataSource());
 
     @Test
     void testfindById() {
-        DataSource ds = HikariDataSourceProvider.getDataSource();
-        ActivityRepository activityRepository = new ActivityRepository(ds);
         Optional<Activity> activity = activityRepository.findById(1L);
         assert(activity.isPresent());
     }
 
     @Test
     void test2findById() {
-        DataSource ds = HikariDataSourceProvider.getDataSource();
-        ActivityRepository activityRepository = new ActivityRepository(ds);
         Optional<Activity> activity = activityRepository.findById(100L);
         assert(activity.isEmpty());
     }
