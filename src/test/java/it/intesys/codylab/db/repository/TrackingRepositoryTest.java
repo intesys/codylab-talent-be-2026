c3ff0d43a1dcf5abeaa2eefcf4d7cdbefa954ceb
DanniaLima <dannia.lima@gmail.com>
2026-06-25 17:37:42 +0200

    test: implement findAll and findById for TrackingRepository

--- src/test/java/it/intesys/codylab/db/repository/TrackingRepositoryTest.java
+++ src/test/java/it/intesys/codylab/db/repository/TrackingRepositoryTest.java
@@ -3,6 +3,7 @@ package it.intesys.codylab.db.repository;
 import it.intesys.codylab.db.config.HikariTestDataSourceProvider;
 import it.intesys.codylab.db.model.Tracking;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Null;
 
 import java.util.List;
 
@@ -19,6 +20,14 @@ class TrackingRepositoryTest {
         assertNotNull(trackings, "La lista non deve essere nulla");
         assertFalse(trackings.isEmpty(), "La lista deve contenere almeno un record.");
 
+
+    }
+    @Test
+    void findById() {
+        Tracking tracking = trackingRepository.findById(1L).orElse(null);
+
+        assertNotNull(tracking, "Dovrebbe esistere un tracciamento con ID 1.");
+        assertEquals(1L, tracking.getId(), "L'ID restituito dovrebbe essere 1.");
 
     }
 }
