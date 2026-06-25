b84fc263e776769ad9490b9fc87154aa85ac341b
Nicolas Taddei <il.taddee@gmail.com>
2026-06-25 11:53:54 +0200

    Activity Repository Test

---
+++ src/test/java/it/intesys/codylab/db/repository/ActivityRepositoryIntTest.java
@@ -0,0 +1,29 @@
+package it.intesys.codylab.db.repository;
+
+import it.intesys.codylab.db.config.HikariTestDataSourceProvider;
+import it.intesys.codylab.db.model.Activity;
+import org.junit.jupiter.api.Test;
+
+import javax.sql.DataSource;
+import java.util.Optional;
+
+class ActivityRepositoryIntTest {
+
+    @Test
+    void testfindById() {
+        DataSource ds = HikariTestDataSourceProvider.getDataSource();
+        ActivityRepository activityRepository = new ActivityRepository(ds);
+        Optional<Activity> activity = activityRepository.findById(1L);
+        assert(activity.isPresent());
+    }
+
+    @Test
+    void test2findById() {
+        DataSource ds = HikariTestDataSourceProvider.getDataSource();
+        ActivityRepository activityRepository = new ActivityRepository(ds);
+        Optional<Activity> activity = activityRepository.findById(100L);
+        assert(activity.isEmpty());
+    }
+}
+
+
