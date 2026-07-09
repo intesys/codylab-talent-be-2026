ad1ad1482febcc5a6125164f34bb605fd6850d5d
DanniaLima <dannia.lima@gmail.com>
2026-07-09 10:03:02 +0200

    feat: expose GET /activities endpoint in ActivityController

---
+++ src/main/java/it/intesys/codylab/controller/ActivityController.java
@@ -0,0 +1,24 @@
+package it.intesys.codylab.controller;
+
+import it.intesys.codylab.db.model.Activity;
+import it.intesys.codylab.db.service.ActivityService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+@RequestMapping("/activities")
+public class ActivityController {
+
+    private final ActivityService activityService;
+
+    public ActivityController(ActivityService activityService) {
+        this.activityService = activityService;
+    }
+
+    @GetMapping
+    public List<Activity> getAll() {
+        return activityService.findAll();
+    }
+}
