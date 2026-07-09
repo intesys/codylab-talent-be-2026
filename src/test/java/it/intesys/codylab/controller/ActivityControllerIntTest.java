9454ed6e225b378992994fdc18121476c1db6fbd
DanniaLima <dannia.lima@gmail.com>
2026-07-09 10:40:28 +0200

    test: add integration test for ActivityController

--- src/test/java/it/intesys/codylab/controller/ActivityControllerIntTest.java
+++ src/test/java/it/intesys/codylab/controller/ActivityControllerIntTest.java
@@ -1,4 +1,23 @@
 package it.intesys.codylab.controller;
 
-public class ActivityControllerIntTest {
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import java.util.List;
+import it.intesys.codylab.db.model.Activity;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class ActivityControllerIntTest {
+
+    @Autowired
+    private ActivityController activityController;
+
+    @Test
+    void shouldReturnListOfActivities() {
+        List<Activity> activities = activityController.getAll();
+        assertNotNull(activities);
+    }
 }
+
