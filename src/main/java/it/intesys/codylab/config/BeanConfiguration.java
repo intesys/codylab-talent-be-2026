3188ff1485f6a35baea557cda155c060db1ab04b
DanniaLima <dannia.lima@gmail.com>
2026-07-09 10:04:38 +0200

    chore: register tracking and activity repositories and services as Spring Beans

--- src/main/java/it/intesys/codylab/config/BeanConfiguration.java
+++ src/main/java/it/intesys/codylab/config/BeanConfiguration.java
@@ -1,8 +1,12 @@
 package it.intesys.codylab.config;
 
 import it.intesys.codylab.db.config.HikariDataSourceProvider;
+import it.intesys.codylab.db.repository.ActivityRepository;
 import it.intesys.codylab.db.repository.ProjectRepository;
+import it.intesys.codylab.db.repository.TrackingRepository;
+import it.intesys.codylab.db.service.ActivityService;
 import it.intesys.codylab.service.ProjectService;
+import it.intesys.codylab.service.TrackingService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,12 +21,34 @@ public class BeanConfiguration {
     }
 
     @Bean
-    public ProjectRepository creteProjectRepository(DataSource dataSource) {
+    public ProjectRepository createProjectRepository(DataSource dataSource) {
         return new ProjectRepository(dataSource);
+    }
+
+    @Bean
+    public ActivityRepository createActivityRepository(DataSource dataSource) {
+        return new ActivityRepository(dataSource);
+    }
+
+    @Bean
+    public TrackingRepository createTrackingRepository(DataSource dataSource) {
+        return new TrackingRepository(dataSource);
     }
 
     @Bean
     public ProjectService createProjectService(ProjectRepository projectRepository) {
         return new ProjectService(projectRepository);
+    }
+
+    @Bean
+    public TrackingService createTrackingService(TrackingRepository trackingRepository,
+                                                 ActivityRepository activityRepository,
+                                                 ProjectRepository projectRepository) {
+        return new TrackingService(trackingRepository, activityRepository, projectRepository);
+    }
+
+    @Bean
+    public ActivityService createActivityService(ActivityRepository activityRepository) {
+        return new ActivityService(activityRepository);
     }
 }
