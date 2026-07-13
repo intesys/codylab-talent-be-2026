6831cb88312a85d22aaf7deb4996c119bbafe838
DanniaLima <dannia.lima@gmail.com>
2026-07-13 10:07:37 +0200

    feat: implement read methods and structure in CustomerService

--- src/main/java/it/intesys/codylab/service/CustomerService.java
+++ src/main/java/it/intesys/codylab/service/CustomerService.java
@@ -1,4 +1,31 @@
-package it.intesys.codylab.service;
+package it.intesys.codylab.db.service;
 
+import it.intesys.codylab.db.model.Customer;
+import it.intesys.codylab.db.repository.CustomerRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    // --- (Dania) ---
+
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+
+    public Optional<Customer> getCustomerById(Long id) {
+        return customerRepository.findById(id);
+    }
+
+    // --- (Michele) ---
+
 }
